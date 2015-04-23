09e9b82da6ed5df38d80962415f0f64794878be0
Julia Ebert <ebertjulia@gmail.com>
2015-04-23 19:41:48 -0400

    Added click baiter

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import clickbaitgen
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    phrase = clickbaitgen.create_bait()
+    return HttpResponse(phrase)
 
 
 def db(request):
